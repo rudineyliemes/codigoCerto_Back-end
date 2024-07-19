863851454bec67339148896997d872c30b1ceaf1
Victor Parizio <victorparizio@hotmail.com>
2024-07-19 18:32:44 -0300

    feat: rota Listar Usuários

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,20 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { UserService } from './user.service';
-import { Prisma, User as UserModel } from '@prisma/client';
+import { Body, Controller, Get, Post } from '@nestjs/common'
+import { UserService } from './user.service'
+import { Prisma, User as UserModel } from '@prisma/client'
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  async signupUser(
+  async createUser(
     @Body() userData: Prisma.UserCreateInput,
   ): Promise<UserModel> {
-    return this.userService.createUser(userData);
+    return this.userService.createUser(userData)
+  }
+
+  @Get()
+  async findAllUsers(): Promise<UserModel[]> {
+    return await this.userService.findAll()
   }
 }
